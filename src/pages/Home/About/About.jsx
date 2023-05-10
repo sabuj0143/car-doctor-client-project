14d9c8bc934ec674fab134d6bad4f01b27ee40a9
sabuj0143 <sabujhowlader011@gmai.com>
2023-05-10 11:14:26 +0600

    Services added

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -12,8 +12,8 @@ const About = () => {
                     <div className='lg:w-1/2 p-4'>
                         <h2 className='text-3xl font-bold text-orange-500'>About Us</h2>
                         <h1 className="text-5xl font-bold mt-4">We are qualified & of experience in this field</h1>
-                        <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
-                        <p className="py-4">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+                        <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+                        <p className="py-4">the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.</p>
                         <button className="btn btn-error">Get More Info</button>
                     </div>
                 </div>
---
+++ src/pages/Home/Service/Service.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import ServiceCart from "./ServiceCart";
+
+const Service = () => {
+
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
+    return (
+        <div className="my-4">
+            <div className="text-center space-y-2">
+                <h3 className="text-2xl font-medium text-orange-600">Service</h3>
+                <h2 className="text-5xl font-semibold text-black">Our Service Area</h2>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {
+                    services.map(service => <ServiceCart
+                    key={service._id}
+                    service={service}
+                    ></ServiceCart>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Service;
